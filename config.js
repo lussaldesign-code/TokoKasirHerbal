505ef40d286a639638820a3a53cc9eda60656990
lussaldesign-code <lussaldesign@gmail.com>
2026-09-23 10:38:45 +0700

    Use compatible Supabase anon key for browser auth

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Hanya gunakan publishable/anon key. Jangan pernah menaruh service_role/secret key di file ini.
 window.APP_CONFIG={
   url:'https://geoedddgvzvqsykuekdw.supabase.co',
-  key:'sb_publishable_8-kbY1H5hvOd56kzMhOEdA_wgQ_2iyW'
+  key:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdlb2VkZGRndnp2cXN5a3Vla2R3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAwODM3MjAsImV4cCI6MjEwNTY1OTcyMH0.DKaoJbKparsGc9_WEQ_jefQVJl5raNgPGckbw9UVoNI'
 };
