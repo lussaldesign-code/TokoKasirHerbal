54361bfa7c90e037d3e3f3d3ca3999ef636b14b4
lussaldesign-code <lussaldesign@gmail.com>
2026-09-24 00:52:58 +0700

    chore: bump app cache for v1.0.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tokokasirherbal-v20260927';
+const CACHE='tokokasirherbal-v20260928';
 const SHELL=['./','./index.html','./config.js','./app.js','./manifest.webmanifest','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
