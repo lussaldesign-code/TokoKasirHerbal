705955ec643aa206446287199fc827d193d9b7d7
lussaldesign-code <lussaldesign@gmail.com>
2026-09-23 14:24:30 +0700

    Add JavaScript Capacitor configuration

---
+++ capacitor.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  appId: 'com.herbalinovasi.tokokasir',
+  appName: 'TokoKasirHerbal',
+  webDir: 'mobile/www',
+  bundledWebRuntime: false,
+  android: {
+    allowMixedContent: false
+  }
+};
