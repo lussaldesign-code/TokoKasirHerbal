72db1a931832e7d3e0ceca1d2bc9fcf8686a0422
lussaldesign-code <lussaldesign@gmail.com>
2026-09-24 10:42:44 +0700

    feat: load Android app from remote web shell

--- capacitor.config.js
+++ capacitor.config.js
@@ -3,6 +3,11 @@ module.exports = {
   appName: 'TokoKasirHerbal',
   webDir: 'mobile/www',
   bundledWebRuntime: false,
+  server: {
+    url: 'https://lussaldesign-code.github.io/TokoKasirHerbal/',
+    cleartext: false,
+    allowNavigation: ['lussaldesign-code.github.io']
+  },
   android: {
     allowMixedContent: false
   }
