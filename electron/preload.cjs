dd4580a4fa2497f8a99e5b51a683985f47945275
lussaldesign-code <lussaldesign@gmail.com>
2026-09-24 16:58:17 +0700

    Add native Panda receipt printer bridge

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -6,5 +6,7 @@ contextBridge.exposeInMainWorld('electronUpdater', {
 
 contextBridge.exposeInMainWorld('electronPrinter', {
   available: true,
-  printReport: html => ipcRenderer.invoke('print-report', html)
+  listPrinters: () => ipcRenderer.invoke('list-printers'),
+  printReport: html => ipcRenderer.invoke('print-report', html),
+  printReceipt: (html, printerName) => ipcRenderer.invoke('print-receipt', { html, printerName })
 });
