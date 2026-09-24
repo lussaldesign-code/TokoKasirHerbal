8ae941fa06583fed09da9730e6e8ad754b3624f9
lussaldesign-code <lussaldesign@gmail.com>
2026-09-24 16:36:51 +0700

    Fix Electron printing bridge

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -1,5 +1,10 @@
-const { contextBridge } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronUpdater', {
   available: true
+});
+
+contextBridge.exposeInMainWorld('electronPrinter', {
+  available: true,
+  printReport: html => ipcRenderer.invoke('print-report', html)
 });
