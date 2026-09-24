a2710b1f352505b2764bcb448be9442463e7e815
lussaldesign-code <lussaldesign@gmail.com>
2026-09-25 01:14:21 +0700

    Brand aplikasi dan software ke TokoKasirLussal

--- capacitor.config.js
+++ capacitor.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appId: 'com.herbalinovasi.tokokasir',
-  appName: 'TokoKasirHerbal',
+  appId: 'com.manajemen-toko-kasir.tokokasir',
+  appName: 'TokoKasirLussal',
   webDir: 'mobile/www',
   bundledWebRuntime: false,
   server: {
-    url: 'https://lussaldesign-code.github.io/TokoKasirHerbal/',
+    url: 'https://lussaldesign-code.github.io/TokoKasirLussal/',
     cleartext: false,
     allowNavigation: ['lussaldesign-code.github.io']
   },
