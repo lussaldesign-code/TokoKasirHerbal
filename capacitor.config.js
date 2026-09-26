cc8c726a228d8060b52741e87f859f1e2515878a
lussaldesign-code <lussaldesign@gmail.com>
2026-09-26 15:06:24 +0700

    Rollback to stable version before latest login fixes

--- capacitor.config.js
+++ capacitor.config.js
@@ -3,9 +3,11 @@ module.exports = {
   appName: 'TokoKasirLussal',
   webDir: 'mobile/www',
   bundledWebRuntime: false,
-  // Android must use the bundled web files produced by build-apk.yml.
-  // Do not point Capacitor at GitHub Pages, otherwise APK builds can keep
-  // loading an older cached web application instead of the current source.
+  server: {
+    url: 'https://lussaldesign-code.github.io/TokoKasirHerbal/',
+    cleartext: false,
+    allowNavigation: ['lussaldesign-code.github.io']
+  },
   android: {
     allowMixedContent: false
   }
